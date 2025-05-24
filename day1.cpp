30f54915f3eea4efc6d0ae4660b1f2c895599299
Yash Kumar Singh <yashsingh1610@gmail.com>
2025-05-25 00:24:24 +0530

    Create day1.cpp

---
+++ day1.cpp
@@ -0,0 +1,28 @@
+/* Day 1:
+Create a Class and Print Attributes (OOPs - Class, Access Specifiers)
+Create a class Person with two public attributes: name (string) and age (int). Create an object
+and print the attributes.
+*Output Example:*
+Name: Alice
+Age: 25     */
+
+#include<iostream>
+using namespace std;
+
+class Person{
+    public :
+      string name;
+      int age;
+
+      void setdata(){
+        cout<<"enter the name: "<<endl;
+        cin>>name;
+        cout<<"enter the age: "<<endl;
+        cin>>age;
+      }
+
+     void getdata(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"Age: "<<age<<endl;
+     }
+};
