844e92d3c11cad96ba49d2fca325ce4956c5bf75
Yash Kumar Singh <yashsingh1610@gmail.com>
2025-06-02 16:35:22 +0530

    Create day8.cpp

---
+++ day8.cpp
@@ -0,0 +1,42 @@
+/* Day 8: Sort Students by Marks using Vector of Objects (STL - sort, custom comparator)
+
+ Create a Student class with name and marks. Store multiple objects in a vector and sort by marks.
+
+*Output Example:*
+Alice: 95
+Bob: 90
+John: 88                   */
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Student {
+public:
+    string name;
+    int marks;
+
+    Student(string n, int m) : name(n), marks(m) {}
+
+    // Static comparator
+    static bool compare(Student &a, Student &b) {
+        return a.marks > b.marks; // Descending
+    }
+};
+
+int main() {
+    vector<Student> students = {
+        Student("Bob", 90),
+        Student("John", 88),
+        Student("Alice", 95)
+    };
+
+    sort(students.begin(), students.end(), Student::compare);
+
+    for (auto &s : students) {
+        cout << s.name << ": " << s.marks << endl;
+    }
+
+    return 0;
+}
